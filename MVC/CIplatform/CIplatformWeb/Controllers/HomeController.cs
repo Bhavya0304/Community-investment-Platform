f65759062cf4f46bc35b6acfbef83da2157d7afe
BhaktiRaval <bhaktiraval112001@gmail.com>
2023-02-13 17:44:36 +0530

    created 4 pages of ci platform

---
+++ MVC/CIplatform/CIplatformWeb/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using CIplatformWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace CIplatformWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+        public IActionResult Forgot_password()
+        {
+            return View();
+        }
+        public IActionResult Register()
+        {
+            return View();
+        }
+        public IActionResult Reset_password()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVC/CIplatform/CIplatformWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CIplatformWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
