d02878b78441d06f7f01107f0023d0e07171cc17
BhaktiRaval <bhaktiraval112001@gmail.com>
2023-02-17 18:03:14 +0530

    grid view

--- MVC/CIplatform/CIplatformWeb/Controllers/HomeController.cs
+++ MVC/CIplatform/CIplatformWeb/Controllers/HomeController.cs
@@ -34,7 +34,10 @@ namespace CIplatformWeb.Controllers
         {
             return View();
         }
-
+        public IActionResult Platform()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
