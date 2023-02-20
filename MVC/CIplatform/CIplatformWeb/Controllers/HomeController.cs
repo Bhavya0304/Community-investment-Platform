4fa6769edecede9fff6500c197de5a9a2b16a4a8
BhaktiRaval <bhaktiraval112001@gmail.com>
2023-02-20 19:04:40 +0530

    list view

--- MVC/CIplatform/CIplatformWeb/Controllers/HomeController.cs
+++ MVC/CIplatform/CIplatformWeb/Controllers/HomeController.cs
@@ -38,6 +38,14 @@ namespace CIplatformWeb.Controllers
         {
             return View();
         }
+        public IActionResult _Grid_View()
+        {
+            return View();
+        }
+        public IActionResult _List_View()
+        {
+            return View();
+        }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
